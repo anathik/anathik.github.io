2c3f50d121a3c3f1db70628f4c2d014b3ebdd79b
Anathi Keswa <me@anathi.io>
2018-01-11 17:53:11 -0500

    Made a saucy slider for the about section- ready for some centered content.. Make sure to have this done asap so the Portfolio section can get some love.

--- web/src/Pages/About/index.js
+++ web/src/Pages/About/index.js
@@ -14,25 +14,63 @@ class About extends Component {
   constructor() {
     super();
     this.state = {
-      someState: "its just an example, dude",
+      currentInfoSelector:"aboutSkillsSelector",
+      selectionBarClassName:"aboutInfoSelectorBar",
+      aboutSliderVisibleContent:"aboutSlider"
     };
+  }
+
+  changeSelectorOnClick() {
+    if (this.state.currentInfoSelector === "aboutSkillsSelector") {
+      this.setState({selectionBarClassName: "aboutInfoSelectorBar"});
+      this.setState({aboutSliderVisibleContent: "aboutSlider"});
+    } else if (this.state.currentInfoSelector === "aboutBioSelector") {
+      this.setState({selectionBarClassName: "aboutInfoSelectorBar-bio"});
+      this.setState({aboutSliderVisibleContent: "aboutSlider-bio"});
+    } else if (this.state.currentInfoSelector === "aboutJobsSelector") {
+      this.setState({selectionBarClassName: "aboutInfoSelectorBar-jobs"});
+      this.setState({aboutSliderVisibleContent: "aboutSlider-interests"});
+    }
+  }
+
+  changeInfoOnClick(className) {
+    if (className !== this.state.currentInfoSelector) {
+      this.state.currentInfoSelector = className;
+      console.log(this.state.currentInfoSelector);
+      this.changeSelectorOnClick();
+    } else {
+      console.log("The state has not changed!");
+    }
   }
 
   render() {
     const { welcomeText } = this.props;
+    
     return ( 
       <div className="about" id="about-div">
         <div className="aboutSectionTitle">
           About
         </div>
         <div className="aboutConditionalMedia" />
-        <div className="aboutInfoSelector" />
+        <div className="aboutInfoSelector">
+          <div className="aboutBioSelector" onClick={this.changeInfoOnClick.bind(this, "aboutBioSelector")}>
+            Bio
+          </div>
+          <div className="aboutSkillsSelector" onClick={this.changeInfoOnClick.bind(this, "aboutSkillsSelector")}>Skills</div>
+          <div className="aboutJobsSelector" onClick={this.changeInfoOnClick.bind(this, "aboutJobsSelector")}>Interests</div>
+          <div className={this.state.selectionBarClassName} />
+        </div>
         <div className="aboutContent">
-          <div className="aboutBio">
-          </div>
-          <div className="aboutSkills">
-          </div>
-          <div className="aboutExperiences">
+          <div className={this.state.aboutSliderVisibleContent}>
+            <div className="aboutBioContent">
+              Oh hi mawrk!
+            </div>
+            <div className="aboutSkillsContent">
+              These are my skills. Just blank.
+            </div>
+            <div className="aboutInterestsContent ">
+              This is my interest- he color black...
+            </div>
           </div>
         </div>
       </div>
